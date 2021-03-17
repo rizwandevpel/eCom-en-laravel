0cb4c7ae0718e1faaf5a40080d32c250667a8ffe
rizwan <rizwan.devp@gmail.com>
2021-03-17 12:14:55 +0500

    header,footer and login pages are done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Latest compiled and minified jquery -->
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    {{ View::make('header') }}
+    @yield('content')
+    {{ View::make('footer') }}
+   
+</body>
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px
+    }
+</style>
+</html>
